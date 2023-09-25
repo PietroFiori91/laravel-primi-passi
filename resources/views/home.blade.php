10327b15c7f7b994c0b2a9d9fa4a0de0f00bb0d3
PietroFiori91 <pederflowers@hotmail.it>
2023-09-25 15:06:15 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Laravel Primi Passi</title>
+</head>
+
+<body>
+    <nav>
+        <ul>
+            <li><a href="{{ route('home') }}">Home</a></li>
+            <li><a href="{{ route('about') }}">About</a></li>
+        </ul>
+    </nav>
+    <h1>Hello World</h1>
+</body>
+
+</html>
